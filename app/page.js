d14eba130aaea7abac3d57fe0a3711d37885e0e2
Albasha99 <albashahmad.hu@gmail.com>
2024-02-09 15:39:18 +0100

    underDevelopment message

--- app/page.js
+++ app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello there!!!</h1>{" "}
+      <h1>Hello there!!!</h1>
+      <h2>
+        Site is currently underdevelopment, if you would like book a session or
+        discuss a request write me at: albashahmad.se@gmial.com
+      </h2>{" "}
     </main>
   );
 }
