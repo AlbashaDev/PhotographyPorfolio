1a114d2d2a2d24177013778959e00eb5a2206ad6
Albasha99 <albashahmad.hu@gmail.com>
2024-02-09 15:52:49 +0100

    css mod.

--- app/page.js
+++ app/page.js
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-evenly p-5">
       <h1>Hello there!!!</h1>
       <h2>
         Site is currently underdevelopment, if you would like book a session or
-        discuss a request write me at: albashahmad.se@gmial.com
-      </h2>{" "}
+        discuss a request write me at: albashahmad.se@gmail.com
+      </h2>
     </main>
   );
 }
