9ca10f0dccebe0994cf44d37c7f5dd80bf568008
Albasha99 <albashahmad.hu@gmail.com>
2024-02-12 00:08:42 +0100

    CheckingDeployedVersion

---
+++ app/contact/page.js
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <main className="flex min-h-screen justify-evenly items-center flex-col p-10">
+      <div>
+        <h1>Email!</h1>
+      </div>
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -2,18 +2,77 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-evenly p-5">
-      <h1>Hello there!!!</h1>
-      <h2>
-        Site is currently underdevelopment, if you would like book a session or
-        discuss a request write me at:{" "}
-        <a
-          href="mailto:albashahmad.se@gmail.com?subject=Inquiry!"
-          className="text-blue-700	no-underline hover:underline"
+    <main className="sm:min-h-screen p-5 sm:py-14 sm:px-10 bg-neutral-700">
+      <section className="bg-stone-200 min-h-[84vh] sm:min-h-[85vh] p-10 rounded-lg">
+        <nav className=" flex  justify-between">
+          {/* Nav on SM Breakpoint = 640px */}
+          <Image
+            className="hidden sm:block -mt-11 -ml-5"
+            src="/A.png"
+            width={130}
+            height={130}
+            alt="logo"
+          />
+          <ul className="hidden sm:block justify-ends flex space-x-4 text-base font-burtons text-neutral-500">
+            <li>
+              <a
+                href="https://calendly.com/albashafoto/30min"
+                className="block hover:text-black
+            "
+              >
+                Book
+              </a>
+            </li>
+          </ul>
+          {/* Nav For Phones */}
+          <Image
+            className=" sm:hidden -mt-11 -ml-5"
+            src="/Name.png"
+            width={130}
+            height={130}
+            alt="logo"
+          />
+        </nav>
+        <div className="p-4 text-neutral-500 font-burtons text-lg">
+          <h1 className="hidden sm:block">Recent Work</h1>
+        </div>
+        {/* Images Carousel */}
+        <div
+          class="carousel-container"
+          className=" overflow-scroll absolute h-[60vh] sm:h-[60vh] border-black border-4 "
         >
-          albashahmad.se@gmail.com
-        </a>
-      </h2>
+          <div class="carousel-slide" className="flex h-[100%] w-[100%]">
+            <Image
+              className=""
+              src="/DSC_1.jpeg"
+              width={1000}
+              height={1000}
+              alt="slide"
+            />
+            <Image
+              className=""
+              src="/DSC_3.jpeg"
+              width={1000}
+              height={1000}
+              alt="slide"
+            />
+            <Image
+              className=""
+              src="/DSC_3.jpeg"
+              width={1000}
+              height={1000}
+              alt="slide"
+            />
+            <Image
+              className=""
+              src="/DSC_4.jpeg"
+              width={1000}
+              height={1000}
+              alt="slide"
+            />
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
